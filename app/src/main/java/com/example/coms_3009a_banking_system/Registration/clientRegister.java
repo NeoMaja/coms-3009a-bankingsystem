a76feeef92e30350cdd59330afff45becc171191
NqobaJele <2105354@students.wits.ac.za>
2021-05-07 12:05:59 +0200

    Added back button for client registration

--- app/src/main/java/com/example/coms_3009a_banking_system/Registration/clientRegister.java
+++ app/src/main/java/com/example/coms_3009a_banking_system/Registration/clientRegister.java
@@ -31,6 +31,7 @@ public class clientRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_register);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 //        assigning variables
 
