d82f14165dc123f6577555a3a57c8405676340c4
NeoMaja <2116176@students.wits.ac.za>
2021-05-08 10:04:49 +0200

    Create Client_Verification_Status.java
    Verification status Ui page

---
+++ app/src/main/java/com/example/coms_3009a_banking_system/Client_Verification_Status.java
@@ -0,0 +1,16 @@
+package com.example.coms_3009a_banking_system;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Client_Verification_Status extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_verification_status);
+
+
+    }
+}
