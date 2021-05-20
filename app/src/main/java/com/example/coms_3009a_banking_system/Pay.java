993e71d64c677df7a3b85d2c291e686b2e852550
ThabetheAndile <70257085+ThabetheAndile@users.noreply.github.com>
2021-05-20 23:58:19 +0200

    Pay

--- app/src/main/java/com/example/coms_3009a_banking_system/Pay.java
+++ app/src/main/java/com/example/coms_3009a_banking_system/Pay.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Pay extends AppCompatActivity {
+
+    // initializing variables
+    EditText recipient,amount,reference,paymentDate;
+    Button pay;
+    RadioButton bankA, bankB, bankC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+
+        // assigning variables
+        recipient = findViewById (R.id.recipient);
+        amount = findViewById(R.id.amount);
+        reference = findViewById(R.id.reference);
+        paymentDate = findViewById(R.id.paymentDate);
+
+        bankA = (RadioButton)findViewById(R.id.toBankA);
+        bankB = (RadioButton)findViewById(R.id.toBankB);
+        bankC = (RadioButton)findViewById(R.id.toBankC);
+
+        pay = (Button)findViewById(R.id.pay);
+
     }
 
     public void onRadioButtonClicked(View view) {
--- app/src/main/java/com/example/coms_3009a_banking_system/Transfer.java
+++ app/src/main/java/com/example/coms_3009a_banking_system/Transfer.java
@@ -1,16 +1,40 @@
 package com.example.coms_3009a_banking_system;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
 
 public class Transfer extends AppCompatActivity {
+
+    // initializing variables
+    EditText transferAmount,transferDate;
+    Button transfer;
+    RadioButton fromChequeAccount, fromInvestmentAccount, fromSavingsAccount,
+            toChequeAccount, toInvestmentAccount, toSavingsAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer3);
+
+        // assigning variables
+        transferAmount = findViewById(R.id.transferAmount);
+        transferDate = findViewById(R.id.transferDate);
+
+        fromChequeAccount = (RadioButton)findViewById(R.id.fromChequeAccount);
+        fromInvestmentAccount = (RadioButton)findViewById(R.id.fromInvestmentAccount);
+        fromSavingsAccount = (RadioButton)findViewById(R.id.fromSavingsAccount);
+
+        toChequeAccount = (RadioButton)findViewById(R.id.toChequeAccount);
+        toInvestmentAccount = (RadioButton)findViewById(R.id.toInvestmentAccount);
+        toSavingsAccount = (RadioButton)findViewById(R.id.toSavingsAccount);
+
+        transfer = (Button)findViewById(R.id.transfer);
+
     }
 
     public void onRadioButtonClicked(View view) {
