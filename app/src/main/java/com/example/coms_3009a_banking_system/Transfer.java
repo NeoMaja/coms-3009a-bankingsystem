c14ea9de9205c29472e9141056198115feb1c117
ThabetheAndile <70257085+ThabetheAndile@users.noreply.github.com>
2021-05-20 23:23:14 +0200

    Transfer

---
+++ app/src/main/java/com/example/coms_3009a_banking_system/Transfer.java
@@ -0,0 +1,18 @@
+package com.example.coms_3009a_banking_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Transfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer3);
+    }
+
+    public void onRadioButtonClicked(View view) {
+    }
+}
