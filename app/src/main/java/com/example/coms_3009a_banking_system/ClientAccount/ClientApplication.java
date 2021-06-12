0adf8da1160aee1fdd85d405cd117af342017803
2106761 <2106761@students.wits.ac.za>
2021-06-12 20:19:38 +0200

    Update Application

--- app/src/main/java/com/example/coms_3009a_banking_system/ClientAccount/ClientApplication.java
+++ app/src/main/java/com/example/coms_3009a_banking_system/ClientAccount/ClientApplication.java
@@ -59,57 +59,6 @@ public class ClientApplication extends AppCompatActivity {
         Email = intent.getStringExtra("email");
         password= intent.getStringExtra("password");
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        //Set home Selected
-        bottomNavigationView.setSelectedItemId(R.id.acc);
-
-        // Perform ItemSelected
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent1;
-                switch (item.getItemId()){
-
-                    case R.id.acc:
-                        return true;
-
-                    case R.id.button_logoutnow:
-                        intent1 = new Intent(ClientApplication.this, Login2.class);
-                        intent1.putExtra("email",Email);
-                        intent1.putExtra("password",password);
-                        startActivity(intent1);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.button_transact:
-                        intent1 = new Intent(ClientApplication.this, Transact.class);
-                        intent1.putExtra("email", Email);
-                        intent1.putExtra("password",password);
-                        startActivity(intent1);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.button_insert:
-                        intent1 = new Intent(ClientApplication.this, Cli_Acc_Test.class);
-                        intent1.putExtra("email",Email);
-                        intent1.putExtra("password",password);
-                        startActivity(intent1);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.button_profile:
-                        intent1 = new Intent(ClientApplication.this, Profile.class);
-                        intent1.putExtra("email", Email);
-                        intent1.putExtra("password", password);
-                        startActivity(intent1);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                }
-                return false;
-            }
-        });
 
     }
 
