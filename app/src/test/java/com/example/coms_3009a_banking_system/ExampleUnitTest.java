b9d9b196ff6c60523ce0636385923e3cf1a63dfa
xolan <2167082@students.wits.ac.za>
2021-06-16 16:09:49 +0200

    unit tests in ExampleUnitTest.java
    Updated three tests.

--- app/src/test/java/com/example/coms_3009a_banking_system/ExampleUnitTest.java
+++ app/src/test/java/com/example/coms_3009a_banking_system/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.example.coms_3009a_banking_system;
 
+import com.example.coms_3009a_banking_system.ClientAccount.Clientitem;
+import com.example.coms_3009a_banking_system.ClientAccount.historyItem;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +12,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
@@ -50,6 +54,66 @@ public class ExampleUnitTest {
         Cno = testUser.getPhoneNo();
         assertEquals("0723456781",Cno);
     }
+
+    @Test
+    public void testClientitem(){
+        Clientitem testUser= new Clientitem("Investment","992218614","500");
+        String  accountType, Amount ,accountNumber;
+        //testing Account Type.
+        accountType = testUser.getmText0();
+        assertEquals("Investment",accountType);
+        //testing account Number.
+        accountNumber = testUser.getmText1();
+        assertEquals("992218614",accountNumber);
+        //Testing Amount.
+        Amount = testUser.getmText2();
+        assertEquals("500",Amount);
+    }
+    @Test
+    public void testhistoryItem(){
+        historyItem testHistory = new historyItem("Car repair","Pay","500","2021-06-16");
+        String Reference,Trans_type,Amount,Date;
+
+        Reference = testHistory.getReference();
+        assertEquals("Car repair",Reference);
+
+        Trans_type = testHistory.getTransaction_type();
+        assertEquals("Pay",Trans_type);
+
+        Amount = testHistory.getAmount();
+        assertEquals("500",Amount);
+
+        Date = testHistory.getDate();
+        assertEquals("2021-06-16",Date);
+
+
+    }
+
+    @Test
+    public void testCreditClass(){
+        CreditClass testHistory = new CreditClass("1988282","Xolani","Nqubezelo","133 Violet street","15000","5000");
+        String First_Name, Last_Name, ID_Number, Residence_Address , Monthly_Expenditure, Monthly_Earnings;
+
+        First_Name = testHistory.getFirst_Name();
+        assertEquals("Xolani",First_Name);
+
+        Last_Name = testHistory.getLast_Name();
+        assertEquals("Nqubezelo",Last_Name);
+
+        ID_Number = testHistory.getID_Number();
+        assertEquals("1988282",ID_Number);
+
+        Residence_Address = testHistory.getResidence_Address();
+        assertEquals("133 Violet street",Residence_Address);
+
+        Monthly_Expenditure = testHistory.getMonthly_Expenditure();
+        assertEquals("5000",Monthly_Expenditure);
+
+        Monthly_Earnings = testHistory.getMonthly_Earnings();
+        assertEquals("15000",Monthly_Earnings);
+
+    }
+
 
 
 }
